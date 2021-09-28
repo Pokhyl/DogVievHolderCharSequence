52c0e41259c7eb68b38e6beb3f52f649ca2bc90b
Vitalii <84464769+Pokhyl@users.noreply.github.com>
2021-09-28 18:48:44 +0200

    Add Dagger

--- app/src/main/java/com/example/dogvievholdercharsequence/Dog.kt
+++ app/src/main/java/com/example/dogvievholdercharsequence/Dog.kt
@@ -1,4 +1,45 @@
 package com.example.dogvievholdercharsequence
 
+import dagger.Component
+import dagger.Module
+import dagger.Provides
+import javax.inject.Inject
+import javax.inject.Singleton
+
 class Dog(var name : String , var age: Int , var breed: String) {
+}
+class Foo @Inject constructor(){
+
+   var list = mutableListOf<Dog>(
+    Dog("Eva", 4, "york"),
+    Dog("sky", 4, "york"),
+    Dog("Barbor", 4, "york"),
+    Dog("Vasy", 4, "york"),
+    Dog("tjjkghg", 4, "york"),
+    Dog("Dog", 4, "york"),
+    Dog("Misha", 4, "york"),
+    Dog("Fruzy", 4, "york"),
+    Dog("Druzhok", 4, "york"),
+    Dog("Sarik", 4, "york"),
+    Dog("Bobik", 4, "york"),
+    Dog("ZhuZhik", 4, "york"),)
+
+    fun getDogList(): MutableList<Dog>{
+        return list
+    }
+}
+
+@Module
+internal object SimpleModule {
+    @Provides
+    fun provideFoo(): Foo {
+        return Foo()
+    }
+}
+
+@Singleton
+@Component(modules = [SimpleModule::class])
+interface SimpleComponent {
+   //fun foo(): Foo?
+    fun inject(activity: MainActivity)
 }
--- app/src/main/java/com/example/dogvievholdercharsequence/MainActivity.kt
+++ app/src/main/java/com/example/dogvievholdercharsequence/MainActivity.kt
@@ -6,27 +6,33 @@ import android.widget.Adapter
 import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dogvievholdercharsequence.databinding.ActivityMainBinding
+import javax.inject.Inject
+
 
 class MainActivity : AppCompatActivity() {
  lateinit var list: MutableList<Dog>
  lateinit var binding: ActivityMainBinding
+ @Inject lateinit var foo: Foo
     override fun onCreate(savedInstanceState: Bundle?) {
+        (applicationContext as MyAplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        list = mutableListOf<Dog>(
-            Dog("Eva", 4, "york"),
-            Dog("sky", 4, "york"),
-            Dog("Barbor", 4, "york"),
-            Dog("Vasy", 4, "york"),
-            Dog("tjjkghg", 4, "york"),
-            Dog("Dog", 4, "york"),
-            Dog("Misha", 4, "york"),
-            Dog("Fruzy", 4, "york"),
-            Dog("Druzhok", 4, "york"),
-            Dog("Sarik", 4, "york"),
-            Dog("Bobik", 4, "york"),
-            Dog("ZhuZhik", 4, "york"),)
+        println( "!!!!!!!!$foo")
+//        list = mutableListOf<Dog>(
+//            Dog("Eva", 4, "york"),
+//            Dog("sky", 4, "york"),
+//            Dog("Barbor", 4, "york"),
+//            Dog("Vasy", 4, "york"),
+//            Dog("tjjkghg", 4, "york"),
+//            Dog("Dog", 4, "york"),
+//            Dog("Misha", 4, "york"),
+//            Dog("Fruzy", 4, "york"),
+//            Dog("Druzhok", 4, "york"),
+//            Dog("Sarik", 4, "york"),
+//            Dog("Bobik", 4, "york"),
+//            Dog("ZhuZhik", 4, "york"),)
+        list = foo.getDogList()
         var adapter = DogAdapter(list)
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(p0: String?): Boolean {
